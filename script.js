96b34f51b8ecc5ac7179579c0640a6834ea216b5
Nikita Kulkarni <nikita.kulkarni@dynamisch.co>
2025-02-05 14:52:21 +0530

    Your descriptive commit message

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function beforeSubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    //for showing date into form
-    let inputdate =  document.querySelector(".inputdate");
-    console.log("inputdate.value",inputdate.value);
-    //convert type into date suing Locale
-    let dateFormate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = dateFormate;
-}
+let captchachecked = false;
+function beforeSubmit(event){
+    if(captchachecked){
+        let outputdate = document.querySelector(".outputdate");
+        //for showing date into form
+        let inputdate =  document.querySelector(".inputdate");
+        console.log("inputdate.value",inputdate.value);
+        //convert type into date suing Locale
+        let dateFormate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = dateFormate;
+    }else{
+        alert("please check the reCaptcha box");
+        event.preventDefault();
+    }
+ }
+    
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
     if (response == null || response.value.trim() == "") {
@@ -16,3 +23,6 @@ function timestamp() {
     }
 } 
  setInterval(timestamp, 500); 
+function captchasucess(){
+      captchachecked = true;
+    }
